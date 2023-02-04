edddb1435344a12b83acf4ceb84f9df2d8ba317b
Son Dang <minhson0506@gmail.com>
2023-02-04 23:31:20 +0200

    add form to upload data

--- src/App.js
+++ src/App.js
@@ -3,22 +3,46 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <>
+      <form
+        action="http://localhost:3001/journeyFile"
+        method="post"
+        enctype="multipart/form-data"
+        class="light-border"
+      >
+        <input class="light-border" type="file" name="file" />
+        <button class="light-border" type="submit">
+          Upload Journey
+        </button>
+      </form>
+      <form
+        action="http://localhost:3001/stationFile"
+        method="post"
+        enctype="multipart/form-data"
+        class="light-border"
+      >
+        <input class="light-border" type="file" name="file" />
+        <button class="light-border" type="submit">
+          Upload Station
+        </button>
+      </form>
+    </>
   );
 }
 
